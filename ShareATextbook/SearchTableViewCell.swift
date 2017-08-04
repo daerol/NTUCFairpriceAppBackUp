2822ae890740fabcd85e2d826803b95d455d1618
Freddy <iamawesome2201@gmail.com>
2017-08-04 14:23:06 +0800

    search

---
+++ ShareATextbook/SearchTableViewCell.swift
@@ -0,0 +1,28 @@
+////
+////  SearchTableViewCell.swift
+////  ShareATextbook
+////
+////  Created by Cheryl Heng on 3/8/17.
+////  Copyright © 2017 Chia Li Yun. All rights reserved.
+////
+//
+//import UIKit
+//
+//class SearchTableViewCell: UITableViewCell {
+//    
+//    @IBOutlet weak var imageView: UIImageView
+//    
+//    
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
