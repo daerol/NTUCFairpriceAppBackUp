be37c6fef5816ea1103c1cbf6f553e29c2664317
Freddy <iamawesome2201@gmail.com>
2017-08-06 00:52:49 +0800

    able to go to next page

---
+++ ShareATextbook/SearchCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SearchCollectionViewCell.swift
+//  ShareATextbook
+//
+//  Created by Cheryl Heng on 5/8/17.
+//  Copyright © 2017 Chia Li Yun. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var itemDetail: UILabel!
+    @IBOutlet weak var itemTag: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var chatButton: UIButton!
+    
+}
--- ShareATextbook/SearchViewController.swift
+++ ShareATextbook/SearchViewController.swift
@@ -12,11 +12,11 @@ class SearchViewController: UIViewController  {
     
     var user:User?
     var postList: [Posting]?
+    private let leftAndRightPadding: CGFloat = 16.0
+    private let numberOfItemsPerRow: CGFloat = 2.0
+    private let heightAdjustment: CGFloat = 80.0
     
     @IBOutlet weak var itemCollectionView: UICollectionView!
-    @IBOutlet weak var pointStackView: UIStackView!
-    @IBOutlet weak var pointLabel: UILabel!
-    @IBOutlet weak var locationStackView: UIStackView!
     @IBOutlet weak var name: UILabel!
     
     @IBOutlet weak var editProfileButton: CustomUIButton!
@@ -62,7 +62,7 @@ class SearchViewController: UIViewController  {
             }
             
             DispatchQueue.main.async(execute: {
-//                self.itemCollectionView.reloadData()
+                self.itemCollectionView.reloadData()
             })
         })
         
@@ -76,15 +76,74 @@ class SearchViewController: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("enter 2")
+        //        return postList!.count
+        
+        if postList != nil {
+            return postList!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = itemCollectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! ItemCollectionViewCell
+        
+        let postStatus = postList![indexPath.row].status
+        
+        cell.itemImage.image = #imageLiteral(resourceName: "textbook")
+        cell.itemTitle.text = postList![indexPath.row].name
+        cell.itemDetail.text = postList![indexPath.row].publisher! + " " + postList![indexPath.row].edition!
+        
+        if postList![indexPath.row].status == "" {
+            cell.itemTag.isHidden = true
+        } else {
+            if postStatus == "D" {
+                cell.itemTag.text = "Donated"
+                cell.itemTitle.textColor = Colors.white
+                cell.itemDetail.textColor = Colors.white
+                cell.bookmarkButton.tintColor = Colors.white
+                cell.chatButton.tintColor = Colors.white
+                cell.backgroundColor = Colors.blue
+            } else if postStatus == "R" {
+                cell.itemTag.text = "Reserved"
+                cell.itemTitle.textColor = Colors.white
+                cell.itemDetail.textColor = Colors.white
+                cell.bookmarkButton.tintColor = Colors.white
+                cell.chatButton.tintColor = Colors.white
+                cell.backgroundColor = Colors.darkRed
+            }
+        }
+        let filePath = postList![indexPath.row].photos[0]
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let url = URL(string: DatabaseAPI.imageDownloadURL + filePath + DatabaseAPI.imageSizeR1000)
+            ImageDownload.downloadImage(url: url!, onComplete: {
+                data in
+                
+                DispatchQueue.main.async() { () -> Void in
+                    //                    cell.itemImage.contentMode = .scaleAspectFit
+                    cell.itemImage.image = UIImage(data: data)
+                }
+            })
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (itemCollectionView.bounds.width - leftAndRightPadding) / numberOfItemsPerRow
+        
+        return CGSize(width: width, height: width + heightAdjustment)
+    }
+
+    
+
 }
 
